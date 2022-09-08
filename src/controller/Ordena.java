addc96a9abfece51ea1d607a32ba4b5135417c4a
hugon <hugon@RODATESUAMAE>
2022-09-07 21:54:14 -0300

    Add mergeSort & quickSort

--- src/controller/Ordena.java
+++ src/controller/Ordena.java
@@ -1,7 +1,9 @@
 package controller;
 
 public class Ordena {
-	public int [] bubblesort(int vet[]) {
+
+//=========================================================================================	
+	public int[] bubbleSort(int vet[]) {
 		int size = vet.length;
 		int i = 0;
 		int aux = 0;
@@ -14,7 +16,85 @@ public class Ordena {
 				}
 			}
 		}
-		return vet; 
+		return vet;
+	}
+
+//=========================================================================================
+
+	public int[] mergeSort(int vet[], int ini, int fim) {
+
+		if (ini < fim) {
+			int meio = (ini + fim) / 2;
+			mergeSort(vet, ini, meio);
+			mergeSort(vet, meio + 1, fim);
+			Intercala(vet, ini, meio, fim);
+		}
+		return vet;
+	}
+
+//=========================================================================================
+
+	private void Intercala(int vet[], int ini, int meio, int fim) {
+		int vetAux[] = new int[vet.length];
+		for (int i = ini; i <= fim; i++) {
+			vetAux[i] = vet[i];
+		}
+		int esq = ini;
+		int dir = meio + 1;
+
+		for (int cont = ini; cont <= fim; cont++) {
+			if (esq > meio) {
+				vet[cont] = vetAux[dir];
+				dir++;
+			} else if (dir > fim) {
+				vet[cont] = vetAux[esq];
+				esq++;
+			} else if (vetAux[esq] < vetAux[dir]) {
+				vet[cont] = vetAux[esq];
+				esq++;
+			} else {
+				vet[cont] = vetAux[dir];
+				dir++;
+			}
+		}
+	}
+
+//=========================================================================================	
+	public int[] quickSort(int vet[], int ini, int fim) {
+		if (fim > ini) {
+			int achaPivo = divideQuick(vet, ini, fim);
+			quickSort(vet, ini, achaPivo - 1);
+			quickSort(vet, achaPivo + 1, fim);
+		}
+		return vet;
+	}
+
+	private int divideQuick(int vet[], int ini, int fim) {
+		int esq = ini + 1;
+		int dir = fim;
+		int pivo = vet[ini];
+
+		while (esq <= dir) {
+			while (esq <= dir && vet[esq] <= pivo) {
+				esq++;
+			}
+			while (dir >= esq && vet[dir] > pivo) {
+				dir--;
+			}
+			if (esq < dir) {
+				troca(vet, esq, dir);
+				esq++;
+				dir--;
+			}
+		}
+		troca(vet, ini, dir);
+		return dir;
+	}
+
+	private void troca(int[] vet, int i, int j) {
+		int aux = vet[j];
+		vet[j] = vet[i];
+		vet[i] = aux;
 	}
 
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -3,14 +3,12 @@ package view;
 import controller.Ordena;
 
 public class Principal {
-	public static void main(String args []) {
+	public static void main(String args[]) {
 		Ordena o = new Ordena();
-		int recebe;
-		int teste [] = {69, 68, 32, 36, 91, 19, 28, 44, 88, 73, 37, 99, 1}; 
-		o.bubblesort(teste);
-		for (int i = 0; i<teste.length; i++) {
-			recebe = teste[i];
-			System.out.println(recebe);
+		int teste[] = { 69, 68, 32, 36, 91, 19, 28, 44, 88, 73, 37, 99, 1 };
+		o.quickSort(teste, 0, teste.length - 1);
+		for (int i = 0; i < teste.length; i++) {
+			System.out.println(i+1 + "# " + teste[i]);
 		}
 	}
 }
